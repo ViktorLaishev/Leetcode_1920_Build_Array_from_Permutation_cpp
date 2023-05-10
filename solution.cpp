f0941e67c9aa86e4ee6987519ee0b326a9fa2501
Viktor Laishev <130911518+ViktorLaishev@users.noreply.github.com>
2023-05-10 17:04:34 +0200

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -4,7 +4,7 @@ public:
         vector<int> ans(nums.size());
         for (int i = 0; i < nums.size(); i++) {
             ans[i] = nums[nums[i]]; 
-        } 
+        }  
         return ans; 
     }
 };
