5222c460cb6a0973213c8218021cdb69bcff5367
Viktor Laishev <130911518+ViktorLaishev@users.noreply.github.com>
2023-05-10 17:04:30 +0200

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> buildArray(vector<int>& nums) {
         vector<int> ans(nums.size());
         for (int i = 0; i < nums.size(); i++) {
-            ans[i] = nums[nums[i]];
+            ans[i] = nums[nums[i]]; 
         } 
         return ans; 
     }
